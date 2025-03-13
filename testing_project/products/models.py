a642de6a855c32f6967a2afe688f6204223aa0c4
신혜란 <shr19970923@gmail.com>
2025-03-13 17:40:26 +0900

    모델 테스트: 데이터베이스 제약조건

--- testing_project/products/models.py
+++ testing_project/products/models.py
@@ -15,6 +15,18 @@ class Product(models.Model):
     def is_stock(self) -> bool: # 당연히 속성이니까 매개변수 x
         return self.stock_count > 0
 
+    class Meta:
+        constraints = [ # 이거 수정하고 반드시 migrations 해줘야 한다: Create constraint price__gt_0 on model product
+            models.CheckConstraint(
+                check=models.Q(price__gt=0),
+                name='price__gt_0'
+            ),
+            models.CheckConstraint(
+                check=models.Q(stock_count__gt=0),
+                name='stock_count__gt_0'
+            ),
+        ] # 음수로 설정하고 .save()하면 django.db.utils.IntegrityError: CHECK constraint failed: stock_count__gt_0 나옴
+
     def get_discounted_price(self, discount_pecentage):
         """할인된 가격을 계산하여 반환"""
         return self.price * (1 - discount_pecentage / 100)
--- testing_project/products/tests/test_models.py
+++ testing_project/products/tests/test_models.py
@@ -1,6 +1,18 @@
 from django.core.exceptions import ValidationError
+from django.db.utils import IntegrityError
 from django.test import TestCase
 from products.models import Product
+
+"""
+database constraints?
+- Product의 price, stock_count는 IntegerField다.
+- 자체 매서드 clean으로 음수가 안되게 유효성검사를 하지만 데이터베이스에 음수가 저장되기는 한다.
+- 그래서 데이터베이스 제약조건이 필요하다!
+
+그러면 clean 매서드는 왜 만든건지?
+- form 제출된 시점에 db에 해당 데이터를 보내기 전에 유효성 검사 오류가 있는 경우
+- 해당 양식을 반환하고 사용자가 오류를 수정할 수 있게 하여 데이터베이스 왕복을 방지하기 위해.
+"""
 
 class ProductModelTest(TestCase):
     def setUp(self):
@@ -25,9 +37,23 @@ class ProductModelTest(TestCase):
         with self.assertRaises(ValidationError): #매개변수인 예상하는 예외가 발생하지 않으면 테스트 실패, 예상된 예외가 아닌 다른 예외 발생 시에도 실패
             self.product.clean()
     
-def test_negative_stock_validation(self):
-        self.product.clean()
-
-        self.product.stock = -1
-        with self.assertRaises(ValidationError): #매개변수인 예상하는 예외가 발생하지 않으면 테스트 실패, 예상된 예외가 아닌 다른 예외 발생 시에도 실패
+    def test_negative_stock_validation(self):
             self.product.clean()
+
+            self.product.stock_count = -1
+            with self.assertRaises(ValidationError): #매개변수인 예상하는 예외가 발생하지 않으면 테스트 실패, 예상된 예외가 아닌 다른 예외 발생 시에도 실패
+                self.product.clean()
+    
+    def test_negative_price_constraint(self):
+        """데이터베이스 제약조건으로 product의 price속성에 음수가 저장이 안되는지 테스트"""
+        product = Product(name='negative price product', price=-1.00, stock_count=10)
+
+        with self.assertRaises(IntegrityError):
+            product.save()
+    
+    def test_negative_stock_count_constraint(self):
+        """데이터베이스 제약조건으로 product의 stock_count속성에 음수가 저장이 안되는지 테스트"""
+        product = Product(name='negative price product', price=1.00, stock_count=-10)
+        
+        with self.assertRaises(IntegrityError):
+            product.save()
